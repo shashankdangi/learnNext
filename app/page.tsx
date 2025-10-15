8c1a1e065985b9e20d2608562902840eaddf37d2
Shashank <sss12212001@outlook.com>
2025-10-15 09:27:57 +0900

    Fixed Bugs

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
-import { lustiana } from "./ui/fonts";
+import { lusitana } from "./ui/fonts";
 
 export default function Page() {
   return (
